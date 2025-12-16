0b2b9fa46997116652bc23b69ec02a79a026acd9
MurielKathleen <murielkathleen@stu.xhu.edu.cn>
2025-12-16 22:12:58 +0800

    Update algorithm4.rs

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -1,12 +1,10 @@
 /*
-	binary_search tree
-	This problem requires you to implement a basic interface for a binary tree
+    binary_search tree
+    This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
-
 
 #[derive(Debug)]
 struct TreeNode<T>
@@ -43,20 +41,28 @@ impl<T> BinarySearchTree<T>
 where
     T: Ord,
 {
-
     fn new() -> Self {
         BinarySearchTree { root: None }
     }
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root {
+            Some(ref mut node) => {
+                node.insert(value);
+            }
+            None => {
+                self.root = Some(Box::new(TreeNode::new(value)));
+            }
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        match &self.root {
+            Some(node) => node.search(value),
+            None => false,
+        }
     }
 }
 
@@ -66,10 +72,56 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                // 插入左子树
+                match self.left {
+                    Some(ref mut left) => left.insert(value),
+                    None => {
+                        self.left = Some(Box::new(TreeNode::new(value)));
+                    }
+                }
+            }
+            Ordering::Greater => {
+                // 插入右子树
+                match self.right {
+                    Some(ref mut right) => right.insert(value),
+                    None => {
+                        self.right = Some(Box::new(TreeNode::new(value)));
+                    }
+                }
+            }
+            Ordering::Equal => {
+                // 二叉搜索树通常不允许重复值
+                // 根据测试要求，对于重复值不做任何操作
+            }
+        }
+    }
+    
+    // Search for a value in the tree
+    fn search(&self, value: T) -> bool {
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                // 在左子树中搜索
+                match &self.left {
+                    Some(left) => left.search(value),
+                    None => false,
+                }
+            }
+            Ordering::Greater => {
+                // 在右子树中搜索
+                match &self.right {
+                    Some(right) => right.search(value),
+                    None => false,
+                }
+            }
+            Ordering::Equal => {
+                // 找到值
+                true
+            }
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -79,24 +131,24 @@ mod tests {
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
 
-        
+        // 初始为空树
         assert_eq!(bst.search(1), false);
 
-        
+        // 插入值
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
+        // 搜索存在的值
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
         assert_eq!(bst.search(7), true);
         assert_eq!(bst.search(2), true);
         assert_eq!(bst.search(4), true);
 
-        
+        // 搜索不存在的值
         assert_eq!(bst.search(1), false);
         assert_eq!(bst.search(6), false);
     }
@@ -105,14 +157,14 @@ mod tests {
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
 
-        
+        // 插入重复值
         bst.insert(1);
         bst.insert(1);
 
-        
+        // 搜索应该成功
         assert_eq!(bst.search(1), true);
 
-        
+        // 检查树结构（应该只有一个节点）
         match bst.root {
             Some(ref node) => {
                 assert!(node.left.is_none());
@@ -121,6 +173,4 @@ mod tests {
             None => panic!("Root should not be None after insertion"),
         }
     }
-}    
-
-
+}
