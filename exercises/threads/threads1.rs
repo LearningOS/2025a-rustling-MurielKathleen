e3fd5ef21fef5c0925a7864ef18e5b52b383910e
MurielKathleen <murielkathleen@stu.xhu.edu.cn>
2025-12-21 16:28:58 +0800

    Update threads1.rs

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,8 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -27,6 +25,11 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        // 使用 join() 等待线程完成并获取返回值
+        match handle.join() {
+            Ok(time) => results.push(time),
+            Err(e) => println!("Thread panicked: {:?}", e),
+        }
     }
 
     if results.len() != 10 {
