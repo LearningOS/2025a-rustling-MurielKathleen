e1fa7e3ded474b05eeb38ff0ea9abccd9dbca059
MurielKathleen <murielkathleen@stu.xhu.edu.cn>
2025-12-21 16:19:21 +0800

    Update strings3.rs

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,28 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // 方法1：使用 format!
+    format!("{} world!", input)
+    
+    // 方法2：使用 String 拼接
+    // input.to_string() + " world!"
+    
+    // 方法3：使用 push_str
+    // let mut s = input.to_string();
+    // s.push_str(" world!");
+    // s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
