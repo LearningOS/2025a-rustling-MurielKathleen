0a5fce6ad4b1e17d04776916cbcc45e3b46b2040
MurielKathleen <murielkathleen@stu.xhu.edu.cn>
2025-12-21 16:42:04 +0800

    Update macros4.rs

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,16 +3,14 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };  // 注意这里的分号
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };  // 这里也要分号（最后一个可选）
 }
 
 fn main() {
